40182e2c087e8a1663028de892cf1e9bf7ce7a8d
emanon001 <emanon001@gmail.com>
2022-07-04 01:14:18 +0900

    feat: `if_chain` crate のサンプルを追加

--- macro-study/src/lib.rs
+++ macro-study/src/lib.rs
@@ -1,1 +1,18 @@
+use if_chain::if_chain;
 mod if_let_chain;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn if_chain() {
+        let opt = Some(1);
+        if_chain! {
+            if let Some(v) = opt;
+            if v == 1;
+            then {
+                assert!(true);
+            }
+        }
+    }
+}
